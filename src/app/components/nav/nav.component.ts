aea4f10a3a969ab551b24fbfff8bf167c40bc5a2
unknown <steve.lorent@gmail.com>
2022-04-25 14:01:43 +0200

    exo 2 menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -14,8 +14,21 @@ export class NavComponent implements OnInit {
   ngOnInit(): void {
     this.listeLien = [
       {title : "Home", url : "home"},
-      {title : "About", url : "about"}
+      {title : "About", url : "about"},
+      {title : "Démos", children : [
+        {title : "D1 - Propiétés", url : "demo/demo1"},
+        {title : "D2 - Bindings", url : "demo/demo2"},
+        {title : "D3 - Pipes", url : "demo/demo3"},
+        {title : "D4 - Directives", url : "demo/demo4"},
+      ]},
+      {title : "Exercices", children : [
+        {title : "Chronomètre", url : "exercices/exo1"}
+      ]}
     ]
+  }
+
+  toggleChildrenVisible(index : number) {
+    this.listeLien[index].isChildrenVisible = !this.listeLien[index].isChildrenVisible
   }
 
 }
